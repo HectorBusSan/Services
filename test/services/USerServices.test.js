46a6a6b73fc3a7bf61d0ef0151273004f301a17b
Hector <hector141599@gmail.com>
2022-05-02 08:15:42 -0500

    3) Agregando Carpetas y Test 1

---
+++ test/services/USerServices.test.js
@@ -0,0 +1,11 @@
+const UserServices= require('./../../app/services/UserServices');
+
+describe('Test for UserServices',()=>{
+    test('1) Create a new using the UserService',()=>{
+        const user = UserServices.create(1,'HéctorBussan',"Héctor");
+        expect(user.username).toBe('HéctorBussan');
+        expect(user.name).toBe('Héctor');
+        expect(user.id).toBe(1);
+        expect(user.bio).not.toBeUndefined();
+    })
+})
