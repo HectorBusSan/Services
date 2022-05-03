2e91819ae55a9bac3696fb9cbca7b88de85f6635
Hector <hector141599@gmail.com>
2022-05-02 19:01:29 -0500

    5) Test 2 completado

--- app/services/UserServices.js
+++ app/services/UserServices.js
@@ -3,5 +3,10 @@ class UserService{
     static create(id,username,name){
         return new User(id,username,name,"Sin Bio");
     }
+    static getInfo(user){
+        let arr = Object.entries(user)
+        let array = [arr[0][1],arr[1][1],arr[2][1],arr[3][1]]
+        return array;
+    }
 }
 module.exports= UserService;
--- test/services/USerServices.test.js
+++ test/services/USerServices.test.js
@@ -8,6 +8,15 @@ describe('Test for UserServices',()=>{
         expect(user.id).toBe(1);
         expect(user.bio).not.toBeUndefined();
     }),
-    // test('2) ')
+    test('2) Get all user data a list',()=>{
+        const user = UserServices.create(1,'HéctorBussan','Héctor');
+        const userInfoInList= UserServices.getInfo(user);
+        expect(userInfoInList[0]).toBe(1);
+        expect(userInfoInList[1]).toBe('HéctorBussan');
+        expect(userInfoInList[2]).toBe('Héctor');
+        expect(userInfoInList[3]).toBe('Sin Bio');
+    })
+    // test("3)",()=>{
 
+    // })
 });
